3e86aa7a99233114d0175025714879e198316239
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-17 10:11:09 +0100

    some changes

--- main.js
+++ main.js
@@ -11,14 +11,20 @@ let playBtn = document.getElementById('play-btn')
 playBtn.addEventListener('click', function(){
 
     for (let i = 1; i <= 100; i++){
+        //creo il div al quale darò classe box
         let box = document.createElement("div")
     
+        //aggiungo la classe box al div creato
         box.classList.add("box")
-    
+
+        //all'interno di box aggiungo il valore di i che è progressivo da 1 a 100
         box.innerHTML = `<span>${i}</span>`;
+
+        //aggiungo box al campo da gioco dopo averlo identificato fuori dalla funzione con un getelementbyid
         campoHtml.append(box)
 
 
+        //al click di box la console stampa il valore di i e aggiunge la classe custom blue che ho nel mio css
         box.addEventListener('click', function () {
         
             console.log(i)
